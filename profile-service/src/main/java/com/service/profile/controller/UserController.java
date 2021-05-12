9d70f39495aad1570f35a99a62dc4b36dda5de58
Dinesh Kumar <dineshgngwr@gmail.com>
2021-05-12 10:53:02 +0530

    Add files via upload

--- profile-service/src/main/java/com/service/profile/controller/UserController.java
+++ profile-service/src/main/java/com/service/profile/controller/UserController.java
@@ -1,6 +1,12 @@
 package com.service.profile.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +16,17 @@ import com.service.profile.model.User;
 @RequestMapping("/services")
 public class UserController {
 
-	@GetMapping("/profile")
-	public User getProfile() {
-		User user = new User(101, "Dinesh Gangwar", "dineshgngwr@gmail.com");
-		return user;
+	List<User> userList = new ArrayList<>();
+	
+	@GetMapping("/profiles")
+	public List<User> getProfile() {
+		return userList;
+	}
+	
+	@PostMapping("/createProfile")
+	@PreAuthorize("hasAuthority('create_profile')")
+	public String createProfile(@RequestBody User user) {
+		userList.add(user);
+		return "User created successfully.";
 	}
 }
